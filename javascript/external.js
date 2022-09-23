1a26043ed68668eac8789c19d2c65a58a4787ef3
lecarver <lcarver@clarku.edu>
2022-09-23 10:48:01 -0400

    Completed Task 3

--- javascript/external.js
+++ javascript/external.js
@@ -1,4 +1,20 @@
+//TASK 2
 //window.alert(5 + 6);
 //window.alert('5 + 6');
-document.write("<h1>A heading</h1>");
-document.write("<p>A sentence.</p>");
+//document.write("<h1>A heading</h1>");
+//document.write("<p>A sentence.</p>");
+
+//TASK 3
+//const x = "web";
+//const y = "mapping";
+//const out = x + y;
+//document.write(out);
+//A prompt box is used to prompt users to input a value before entering a page.
+user_name = window.prompt("Please enter your name", "Type your name here");
+document.write(user_name);
+
+//There are many ways to use the feature prompt
+sign = window.prompt(); //opens blank prompt
+sign = prompt(); //opens blank prompt
+sign = window.prompt("Are you feeling lucky"); //open prompt with this text
+sign = window.prompt("Are you feeling lucky", "sure"); //open the window with the text & default value.
