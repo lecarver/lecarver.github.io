7a8376ab94fdc7f445748e8cbe144301069dfadd
lecarver <lcarver@clarku.edu>
2022-09-30 11:03:28 -0400

    Automated table creation for webmap reviews

--- javascript/external.js
+++ javascript/external.js
@@ -9,6 +9,18 @@ message = "<hi>Hello, welcome to my webpage, "+ user_name + "!</h1>";
 
 return message;
 }
+
+function webmap_table(){
+  document.write("<table width = 100%>");
+  for(var row = 0; row < 2; row++){
+    document.write("<tr>");
+    for(var column = 0; column < 3; column++){
+      document.write("<td>" + row + "," + column + "</td>");
+    }
+    document.write("</tr>");
+  }
+  document.write("</table>");
+}
 //document.write(welcome());
 
 
